2de90d2df84d063aed919ffc3f28a9ca499c5950
AndreyVolokhatyy <ftrixagain@gmail.com>
2024-03-26 21:02:55 +0300

    fix: implementation, add constructor

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exceptions.SQLRequestExceptions;
